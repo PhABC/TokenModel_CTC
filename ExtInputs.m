d832c30dd41fb017540847c3f0bfa916df0b4599
Ph.C <ph.castonguay@gmail.com>
2016-01-29 13:47:30 -0500

    Additive urgency signal

--- ExtInputs.m
+++ ExtInputs.m
@@ -5,18 +5,18 @@ function V = ExtInputs(S,Stim)
 %% Stimuli
 
 %Unpacking fields
-   N=S.N; T=S.T;  nbEx=S.nbEx; c=S.c; hnorm=S.hnorm; jumpT=S.jumpT;
-   start=S.start; stimW=S.stimW;
-   
+   T=S.T;  nbEx=S.nbEx; c=S.c;jumpT=S.jumpT; bias=S.bias; Uslop=S.Uslop;
+   onset=S.onset; stimW=S.stimW; Utype=S.Utype; Uw=S.Uw;  Uori = S.Uori;
+     
 %Initializing
-   nstim   = Stim.nbStim{c};              %Number of stim of chosen c
+   nstim   = Stim.nbStim{c};               %Number of stim of chosen c
    stimRaw = Stim.stimRawR(Stim.idx{c},:); %Trials foc condition c
    
 %Logic matrix to expand stimuli wrt T
    timeLogiMat = zeros(15,T);
    i = 1;
    
-   for bin = start:jumpT:T
+   for bin = onset:jumpT:T
        if i < 15
          timeLogiMat(i,bin:bin+jumpT-1) = 1;
        else
@@ -26,20 +26,39 @@ function V = ExtInputs(S,Stim)
        i = i+1;
    end
    
-   stimAll = stimRaw*timeLogiMat; %Stimuli from c expanded wrt T
+   stimAll = stimRaw*timeLogiMat;   %Stimuli from c expanded wrt T
    
 %Choosing examples
-   idx   = randi(nstim,[nbEx,1]); % Select nbEx random integer within nstim
-   stim  = stimAll(idx,:)*stimW;
+   idx     = randi(nstim,[nbEx,1]); %Select nbEx random integer within nstim
+   stim    = stimAll(idx,:);
    
-   %Creating stim for simulation
-   
-  asdfasdf
-       
   
+%% Urgency   
+
+%Baseline urgency signal
+   Uend = (T-onset)*Uslop;       %Last point of linear function wrt T
+   urg  = zeros(nbEx,T);  
+   urg(:,onset:end) = repmat(linspace(0,Uend,T-onset+1),... 
+                             [nbEx,1]); %Urgency starts at stim onset
+   urg  = urg/max(urg(:,end));                             % Normalized
    
+%Creating gauss distribution   
+   urg  = urg .* abs(repmat(randn(nbEx,1)+Uslop,1,T))...  % Random slopes
+               + repmat(0.1*randn(nbEx,1)+Uori,1,T);      % Random origins
+   urg  = urg/max(urg(:,end));                            % Normalized
+   urg(urg<0) = 0;                                        % Del values < 0
+
    
-   
+   if Utype == 1
+       %Additive urgency
+        V = stim*stimW  + urg*Uw + bias;
+
+   elseif Utype ==2
+       %Multiplicative urgency ~~~ NOT MULTIPLIYING ALL INPUTS CURRENTLY
+        V = stim*stimW .* urg*Uw + bias;
+       
+   end
+ 
     
 %% Simple stimuli JPT    
 % %easy, non-misleading stimulus
