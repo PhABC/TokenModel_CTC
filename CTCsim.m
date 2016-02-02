a4f3a61d18b0957705cedb642000224cf63963e7
Ph.C <ph.castonguay@gmail.com>
2016-02-02 12:09:51 -0500

    Adding stimuli, bias, urgency and noise inputs

--- CTCsim.m
+++ CTCsim.m
@@ -3,74 +3,70 @@ function [M1,M2] = CTCsim(S)
 
 %Unpacking fields
 N = S.N; T = S.T; dt = S.dt; tau = S.tau; alpha = S.alpha; beta = S.beta;
-gamma = S.gamma; eta = S.eta; Tau = S.Tau; 
+gamma = S.gamma; eta = S.eta; Tau = S.Tau; Utype = S.Utype;
 
 
-
-
+% Initialization
+    Y1 = zeros(N,N);
+    Y2 = zeros(N,N);
+    
     X1 = zeros(N,1);
     X2 = zeros(N,1);
     M1 = zeros(N,T);
     M2 = zeros(N,T);
-
-    Y1 = zeros(N,1);
-    Y2 = zeros(N,1);
 
 
     for s=1:T/dt
         t = (s-1)*dt;        % Current time in ms
 
         %activation from PMd1
-        s_wY1 = zeros(N,1);
-        for j = 1:N
-            s_wY1 = s_wY1 + S.w1(:,j).*Y1;
-        end
+        s_wY1 = sum(S.w1.*Y1,2);
 
         %activation from PMd2
-        s_wY2 = zeros(N,1);
-        for j = 1:N
-            s_wY2 = s_wY2 + S.w2(:,j).*Y2;
-        end
+        s_wY2 = sum(S.w2.*Y2,2);
 
-        %within-layer activation 1
-        s_KE1 = zeros(N,1);
-        for j = 1:N
-            s_KE1 = s_KE1 + S.KE(:,j) .* fct(Y1(j));  %nonlinear
-    %           s_KE1 = s_KE1 + S.KE(:,j) .* Y1(j);  
-        end
+        %within-layer activation 1    
+        s_KE1 = sum(S.KE.*fct(Y1),2); % Nonlinear fct
+%       s_KE1 = sum(S.KE.*Y1,2);      % Linear
 
         %within-layer activation 2
-        s_KE2 = zeros(N,1);
-        for j = 1:N
-    %         s_KE2 = s_KE2 + S.KE(:,j) .* f(Y2(j)); %nonlinear
-              s_KE2 = s_KE2 + S.KE(:,j) .* Y2(j);
-        end
+        s_KE2 = sum(S.KE.*fct(Y2),2); % Nonlinear fct
+%       s_KE2 = sum(S.KE.*Y2,2);      % Linear
+        
+        
+        s_I1 = sum(S.KI .* fct(Y1),2);
+        s_I2 = sum(S.KI .* fct(Y2),2);
+        
+        %% ???
+        
+        Y1 = repmat(max(X1-Tau,0),1,N); %% ???
+        Y2 = repmat(max(X2-Tau,0),1,N); %% ???
 
-        s_I1 = zeros(N,1);
-        for j = 1:N
-            s_I1 = s_I1 + S.KI(:,j) * fct(Y1(j));
-        end
-
-        s_I2 = zeros(N,1);
-        for j = 1:N
-            s_I2 = s_I2 + S.KI(:,j) * fct(Y2(j));
-        end
-
-        Y1 = max(X1-Tau,0);
-        Y2 = max(X2-Tau,0);
-
-        E1 = S.V(:,s) + s_wY2 + s_KE1;   
+        E1 = s_wY2 + s_KE1 + S.V(:,s);   
         E2 = s_wY1 + s_KE2;
 
-        dX1 = -(alpha.*X1) + (beta - X1).*gamma.*E1 - X1.*s_I1+(eta.*randn(N,1));
-        dX2 = -(alpha.*X2) + (beta - X2).*gamma.*E2 - X2.*s_I2+(eta.*randn(N,1));
-        dX1 = dX1.*tau;
-        dX2 = dX2.*tau;
+        dX1 = -(alpha.*X1) + (beta - X1).*gamma.*E1 - X1.*s_I1;
+        dX2 = -(alpha.*X2) + (beta - X2).*gamma.*E2 - X2.*s_I2;
+        
+        if Utype == 1 %additive urgency 
+            
+            dX1 = ( dX1 + S.U(:,s) ) .* tau;
+            dX2 = ( dX2 + S.U(:,s) ) .* tau;
+        
+        elseif Utype == 2 %Multiplicative urgency
+        
+            dX1 = ( dX1 .* S.U(:,s) ) .* tau;
+            dX2 = ( dX2 .* S.U(:,s) ) .* tau;    
+        
+        end
+        
         X1 = X1 + dX1.*dt;
         X2 = X2 + dX2.*dt;
+        
         M1(:,s) = X1;
         M2(:,s) = X2;
 
     end
 
-end
+
+
---
+++ TrialInput.m
@@ -0,0 +1,58 @@
+function [ V,U ] = TrialInput(S,trial)
+%% TrialInput
+% Will put all inputs in right format for each trial in the simulation   
+
+%Unpacking fields
+  T=S.T; N = S.N; nbEx=S.nbEx; onset=S.onset; stimW=S.stimW; Utype=S.Utype;
+
+%% Stimuli and orientation preference
+%Orientation for token 
+  sd     = 30;      % Standart deviation of token orientation
+  m      = 180;     % Mean for center of direction 
+
+  %Because stimuli are opposite directions, the left is represented with
+  %negative values and the right stim are positive. If values have same
+  %sign as the actual stimuli, then the stim value will be positive,
+  %otherwise it will be negative. Removing negative values later.
+  
+  gaussL = -Gauss2d([360,1],[0,m],sd,1);  % 1D Distribution - Left angle    
+  gaussR = -circshift(gaussL,-m);         % Translation for right angle
+    
+  tokG   = gaussL + gaussR;               % Combining left & right  
+
+%Neurons preference with token orientation
+  hstim  = S.hnorm .* repmat(tokG,1,N);             
+  
+%Cummulative input for each neurons based on stim and pref 
+  stim   = hstim'   * repmat(S.stim(trial,:),360,1);
+
+%Negative values indicate their non prefered direction
+  stim(stim<0) = 0;
+  
+  asfasf
+  
+%% Fast noise
+  FG = randn(N,T);
+
+%% Bias, fast noise and slow noise
+  V  = stim + S.bias + repmat(S.SG(trial,:),N,1)*S.sG + FG*S.fG;
+  U  = repmat(S.urg(trial,:),N,1);
+  
+  
+  
+ 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+   
+   
+   
+   
+   
