7792a37e1100ea3b28e776c3899d5872ca995301
Ph.C <ph.castonguay@gmail.com>
2016-02-18 16:23:05 -0500

    Option for random slope

--- Functions/ExtInputs.m
+++ Functions/ExtInputs.m
@@ -39,10 +39,13 @@ function [ urg,stim,SG ] = ExtInputs(S,Stim)
    urg(:,onset:end) = repmat(linspace(0,Uend,T-onset+1),... 
                              [nbEx,1]);      % Urgency starts at stim onset
 %    urg  = urg/max(urg(:,end));                             % Normalized
-   
+
+
 %Creating gauss distribution   
+if S.Urand
    urg  = urg .* abs(repmat(randn(nbEx,1)+Uslop,1,T))...  % Random slopes
                + repmat(0.1*randn(nbEx,1)+Uori,1,T);      % Random origins
+end
 %    urg  = urg/max(urg(:,end));                            % Normalized
    urg(urg<0) = 0;                                        % Del values < 0
 
