8eef960d9375fad2324550d9d5c126ce56edb920
PhABC <ph.castonguay@gmail.com>
2016-04-10 13:29:11 -0400

    Generating distribution for parameters

---
+++ Functions/randParam.m
@@ -0,0 +1,20 @@
+function Y = randParam(X,std,N)
+%% randparam 
+% This function will output random values centered around the values 
+% provided in X with standart deviation of 'std'. The output will be a 
+% matrix Y of dimensions (length(X) by N)
+
+d1 = length(X);
+
+% Row vector condition
+if size(X,1)>size(X,2); X = X'; end
+
+% Repmat X for vector operations
+X = repmat(X,N,1);
+
+% Random values
+R = randn(N,d1)*std;
+    
+% Gaussian distributed parameters  
+Y = X + X.*R;
+    
